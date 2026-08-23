03856deda27587e87971cb9ef90d330518b1f12b
MOQ <steiblekeanu@gmail.com>
2026-08-23 19:14:01 +0200

    was du geändert hast

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import { Analytics } from '@vercel/analytics/next'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   )
